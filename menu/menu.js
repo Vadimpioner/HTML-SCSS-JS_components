82aef2bfd661bf0ddf6469d92e7b69313340fc82
Vadim <57544626+Vadimpioner@users.noreply.github.com>
2021-07-17 17:37:18 +0300

    Add files via upload

---
+++ menu/menu.js
@@ -0,0 +1,33 @@
+function menuAdaptive(hamburger, menu, menuClose, menuLink) {
+    hamburger = document.querySelector(hamburger)
+    menu = document.querySelector(menu)
+    menuClose = document.querySelector(menuClose)
+    menuLink = document.querySelectorAll(menuLink);
+
+    hamburger.addEventListener('click', () => {
+        menu.classList.add('active');
+        document.body.style.overflow = 'hidden'; // преостанавливаем скролл при появлении модального окна
+    });
+
+    menuClose.addEventListener('click', () => {
+        menu.classList.remove('active');
+        document.body.style.overflow = '';
+    });
+    
+    menuLink.forEach(item => {
+        item.querySelector("a").addEventListener('click', () => {
+            menu.classList.remove('active');
+            document.body.style.overflow = '';
+    
+        });
+    });
+
+    document.addEventListener('keydown', (e) => { // событие keydown срабатывает при нажатии кнопок
+        if (e.code === 'Escape' && menu.classList.contains('active')) {
+            menu.classList.remove('active');
+            document.body.style.overflow = '';
+        }
+    });
+}
+
+menuAdaptive('.hamburger', '.menu', '.menu__close', '.menu__link')
